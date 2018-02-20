4fd5561a314cbfdf35d812cc646aada5e33bfc3a
MarySorok <32310821+MarySorok@users.noreply.github.com>
2018-02-20 22:52:20 +0300

    add classes and structures

--- PolinomInList_Nproj/PolinomInList_lib/polinom.h
+++ PolinomInList_Nproj/PolinomInList_lib/polinom.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "ringlist.h"
+
+template <typename T>
+class Polinom {
+	string s;
+private:
+	RingList<T> *monoms;
+public:
+	Polinom() {};
+	Polinom(const string &s1);
+	Polinom(const Polinom &p1);
+	~Polinom();
+	Polinom operator+(const Polinom &p1);
+	Polinom operator*(const Polinom &p1);
+	*const operator+(const double d);
+	const Polinom& operator=(const Polinom &p1);
+};
--- PolinomInList_Nproj/PolinomInList_lib/ringlist.h
+++ PolinomInList_Nproj/PolinomInList_lib/ringlist.h
@@ -1,1 +1,36 @@
 #pragma once
+ 
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class Monom {
+	double cf;
+	unsigned int abc;
+};
+
+template <typename T>
+class Link {
+public:
+	T data;
+	Link* pNext;
+};
+
+template <typename T>
+class RingList {
+private:
+	Link<T> *head, *current;
+public:
+	RingList();
+	RingList(const RingList<T> &l1);
+	~RingList();
+	void Reset() { current = head; }
+	bool IsEnded();
+	Link* GetNext() { current = current->pNext; }
+	void InsertToTail(const T& d); // вставка в конец
+	void Insert(const T& d); // вставка в упорядоченный список
+	void Sequencing(); // упорядочивание
+	RingList Merge(const RingList<T> &l1); // слияние 2-ух списков в 3-ий
+};
