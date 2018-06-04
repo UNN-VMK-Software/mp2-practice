d0bdc8f52f9ec00ee588c75a0a14f56dc511036a
dmtrii <dmitrijkaganov@ip-1-172.unn.ru>
2018-06-04 11:30:18 +0300

    Amanded postfix.h

--- KaganovDA/lab1/include/postfix.h
+++ KaganovDA/lab1/include/postfix.h
@@ -13,9 +13,11 @@ class postfix
 private:
 	static int GetOperationPtr(char op);				//получить приоретет операции
 	static int IsOperation(char op);					//проверка на знак операции
-public:
 	static bool Operand(const char Exp);
 	static float calc_op(float one, float two, char op);
+public:
+	/* static bool Operand(const char Exp);
+	static float calc_op(float one, float two, char op); */
 	static char* ConvertToPolish(char *Exp, int len);				//преобразование в польскую форму
 	static float ConvertToInfix(char* Exp, int len);				//перевод из постфиксной формы и подсчёт выражения
 };
